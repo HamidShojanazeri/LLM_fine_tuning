a5390f0b8f0765ccc351d0a241646fec63f78102
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-05-22 02:54:34 +0000

    adding configs

---
+++ configs/__init__.py
@@ -0,0 +1,1 @@
+from .peft import lora_config, llama_adapter_config, prefix_config
---
+++ configs/peft.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+from typing import ClassVar
+
+@dataclass
+class lora_config:
+     r: int=16
+     lora_alpha: int=32
+     target_modules: ClassVar[list[str]]= ["q_proj", "v_proj"]
+     bias= "none"
+     task_type: str= "CAUSAL_LM"
+     lora_dropout: float=0.05
+
+@dataclass
+class llama_adapter_config:
+     adapter_len: int= 10
+     adapter_layers: int= 30
+     task_type: str= "CAUSAL_LM"
+
+@dataclass
+class prefix_config:
+     num_virtual_tokens: int=30
+     task_type: str= "CAUSAL_LM"    
