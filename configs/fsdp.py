54eb91ef64f2315a1c29bd40de417f76216185d3
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-05-23 23:21:26 +0000

    adding configs

---
+++ configs/fsdp.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from typing import ClassVar
+from torch.distributed.fsdp import ShardingStrategy
+
+@dataclass
+class fsdp_config:
+    mixed_precision: bool=True
+    use_fp16: bool=True
+    seed: int=42
+    sharding_strategy: ShardingStrategy = ShardingStrategy.FULL_SHARD
+    
+    
+    
