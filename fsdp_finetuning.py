788d065aba274920cd5ee5c117d93f51b3e69294
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-08 02:59:34 +0000

    PEFT Wrapping policy

--- fsdp_finetuning.py
+++ fsdp_finetuning.py
@@ -7,11 +7,14 @@ import torch
 import transformers
 from datasets import load_dataset
 import os.path as osp
+from tqdm import tqdm
 """
 Unused imports:
 import torch.nn as nn
 import bitsandbytes as bnb
 """
+import torch._dynamo
+torch._dynamo.config.suppress_errors = True
 
 from peft import (
     LoraConfig,
@@ -20,13 +23,14 @@ from peft import (
     prepare_model_for_int8_training,
     set_peft_model_state_dict,
 )
+from utils import fsdp_auto_wrap_policy
 from transformers import LlamaForCausalLM, LlamaTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoTokenizer
 import torch.distributed as dist
 from utils.generation_utils import Prompter, generate_and_tokenize_prompt, tokenize
 
 from utils.train_utils import set_tokenizer_params, train, evaluation
 
-from utils.dataset_utils import get_sharded_datasets
+from utils.dataset_utils import get_sharded_datasets, InstructionDataset
 from peft import get_peft_config, get_peft_model, PrefixTuningConfig, TaskType, PeftType, AdaptionPromptConfig
 import configs
 from torch.distributed.fsdp import (
@@ -44,6 +48,8 @@ from pkg_resources import packaging
 import torch
 import torch.cuda.nccl as nccl
 import torch.distributed as dist
+from transformers.models.t5.modeling_t5 import T5Block
+from transformers.models.llama.modeling_llama import LlamaDecoderLayer
 
 verify_bfloat_support = (
     torch.version.cuda
@@ -104,9 +110,9 @@ def get_policies(cfg, rank):
     # print(f"**overriding mp to fp16 - remove")
     # mixed_precision_policy = policies.fpSixteen
 
-    wrapping_policy = policies.get_t5_wrapper()
+    # wrapping_policy = policies.get_t5_wrapper()
 
-    return mixed_precision_policy, wrapping_policy
+    return mixed_precision_policy #, wrapping_policy
 
 def get_parameter_dtypes(model):
     parameter_dtypes = {}
@@ -114,11 +120,12 @@ def get_parameter_dtypes(model):
         parameter_dtypes[name] = parameter.dtype
     return parameter_dtypes
       
-def train(
+def main(
     # model/data params
     base_model: str = "",  # the only required argument
-    data_path: str = "yahma/alpaca-cleaned",
+    data_path: str = "/data/home/hamidnazeri/stanford_alpaca/alpaca_data.json",
     output_dir: str = "./lora-alpaca",
+    model_path = "/data/home/hamidnazeri/LLM_fine_tuning/model/models--decapoda-research--llama-7b-hf/snapshots/5f98eefcc80e437ef68d457ad7bf167c2c6a1348/",
     # training hyperparams
     batch_size: int = 128,
     micro_batch_size: int = 4,
@@ -150,8 +157,7 @@ def train(
             train_config.model_name
         ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
         
-    #setup process group
-    setup()
+    
     torch.cuda.manual_seed(fsdp_config.seed)
     torch.manual_seed(fsdp_config.seed)
 
@@ -160,8 +166,8 @@ def train(
     rank = int(os.environ["RANK"])
     world_size = int(os.environ["WORLD_SIZE"])
     
-    if torch.distributed.is_initialized():
-        torch.cuda.set_device(local_rank)
+    #setup process group 
+  
      
     
     if peft_method=="lora":
@@ -192,8 +198,8 @@ def train(
     
     gradient_accumulation_steps = batch_size // micro_batch_size
 
-    device_map = "auto"
-    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    # device_map = "auto"
+    # world_size = int(os.environ.get("WORLD_SIZE", 1))
     # ddp = world_size != 1
     # if ddp:
     #     device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
@@ -201,12 +207,13 @@ def train(
 
 
 
-    model = AutoModelForSeq2SeqLM.from_pretrained(
+    model = LlamaForCausalLM.from_pretrained(
         train_config.model_name,
         # load_in_8bit=True,
         # torch_dtype=torch.float16,
         # device_map=device_map,
     )
+    model.to(torch.bfloat16)
     if rank==0:
         parameter_dtypes = get_parameter_dtypes(model)
         for name, dtype in parameter_dtypes.items():
@@ -214,9 +221,9 @@ def train(
         
     # model.to(torch.float32)
 
-    tokenizer = AutoTokenizer.from_pretrained(train_config.model_name)
+    # tokenizer = LlamaTokenizer.from_pretrained(train_config.model_name)
 
-    set_tokenizer_params(tokenizer)
+    # set_tokenizer_params(tokenizer)
     
 
     # model = prepare_model_for_int8_training(model)
@@ -225,15 +232,21 @@ def train(
         task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
     )
     model = get_peft_model(model, peft_config)
-    model.train()
     model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
     
-    #Getting fsdp configs
-    mp_policy, wrapping_policy = get_policies(fsdp_config, rank)
+    setup()
     
+    if torch.distributed.is_initialized():
+        torch.cuda.set_device(rank)
+    
+    #Getting fsdp configs
+    mp_policy = get_policies(fsdp_config, rank)
+    my_auto_wrapping_policy = fsdp_auto_wrap_policy(model, LlamaDecoderLayer)
+    mp_policy = None
     model = FSDP(
         model,
-        auto_wrap_policy=wrapping_policy,
+        use_orig_params=True,
+        auto_wrap_policy=my_auto_wrapping_policy,
         mixed_precision=mp_policy,
         sharding_strategy=fsdp_config.sharding_strategy,
         device_id=torch.cuda.current_device(),
@@ -241,33 +254,65 @@ def train(
         # param_init_fn=my_init_fn
     )
 
-    shard_dataset_train, shard_dataset_val = get_sharded_datasets(data_path, val_set_size, num_shards)
+    # shard_dataset_train, shard_dataset_val = get_sharded_datasets(data_path, val_set_size, num_shards)
+    
+    dataset_train = InstructionDataset(
+        data_path=data_path, model_path=model_path, max_words=224, partition="train"
+    )
+    dataset_val = InstructionDataset(
+        data_path=data_path, model_path=model_path, max_words=224, partition="val"
+    )
+    
     
     train_sampler = DistributedSampler(
-        shard_dataset_train, rank=dist.get_rank(), num_replicas=dist.get_world_size(), shuffle=True
+        dataset_train, rank=dist.get_rank(), num_replicas=dist.get_world_size(), shuffle=True
     )
 
     if train_config.run_validation:
         val_sampler = DistributedSampler(
-            shard_dataset_val, rank=dist.get_rank(), num_replicas=dist.get_world_size()
+            dataset_val, rank=dist.get_rank(), num_replicas=dist.get_world_size()
         )
+ 
     train_dataloader = torch.utils.data.DataLoader(
-        shard_dataset_train,
+        dataset_train,
         batch_size=train_config.batch_size_training,
         num_workers=train_config.num_workers_dataloader,
         pin_memory=False,
         sampler=train_sampler,
+        drop_last=True,
+        # collate_fn = data_collator,
     )
 
     if train_config.run_validation:
         eval_dataloader = torch.utils.data.DataLoader(
-            shard_dataset_val,
+            dataset_val,
             batch_size=train_config.val_batch_size,
             num_workers=train_config.num_workers_dataloader,
             pin_memory=False,
             sampler=val_sampler,
+            drop_last=True,
+            # collate_fn = data_collator,
         )
-
+    for step, (examples, labels, example_mask) in enumerate(tqdm(train_dataloader)):
+                print(f"================== type(batch) : {type(examples)}, len(batch): {len(examples)}, actual example {examples.size()}===================")
+                inputs = {'input_ids': examples, 'attention_mask': example_mask, 'labels': labels}
+                outputs = model(**inputs)
+                print("we could run the inference ******************")
+    # output_dir = "./"
+    # trainer = transformers.Trainer(
+    #     model=model,
+    #     train_dataset=shard_dataset_train,
+    #     eval_dataset=shard_dataset_val,
+    #     args=transformers.TrainingArguments(
+    #         per_device_train_batch_size=train_config.val_batch_size,
+    #         output_dir=output_dir,
+    #     ),
+    #     data_collator=transformers.DataCollatorForSeq2Seq(
+    #         tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+    #     ),
+    # )
+    
+    # train_dataloader = trainer.get_train_dataloader()
     # if resume_from_checkpoint:
     #     # Check the available weights and load them
     #     checkpoint_name = os.path.join(
@@ -302,4 +347,4 @@ def train(
 
 
 if __name__ == "__main__":
-    fire.Fire(train)
+    fire.Fire(main)
