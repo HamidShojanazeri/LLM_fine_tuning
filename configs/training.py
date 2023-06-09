ae0cfeb5022781e045dda3f3f74e7dd96ef550bc
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-06-09 01:44:50 +0000

    Added cnn_dm dataset

--- configs/training.py
+++ configs/training.py
@@ -7,16 +7,17 @@ class train_config:
     model_name: str="decapoda-research/llama-7b-hf"
     run_validation: bool=True
     batch_size_training: int=4
-    num_workers_dataloader: int=2
+    num_workers_dataloader: int=1
     lr: float=2e-4
     weight_decay: float=0.0
     gamma: float= 0.85
     use_fp16: bool=False
     mixed_precision: bool=True
     val_batch_size: int=4
-    dataset: str="grammer_dataset"
-    dataset_train: str = "grammer_dataset/gtrain_1k.csv"  # grammar_13k.csv
-    dataset_test: str = "grammer_dataset/grammar_validation.csv"
+    # dataset: str="grammer_dataset"
+    # dataset_train: str = "grammer_dataset/gtrain_1k.csv"  # grammar_13k.csv
+    # dataset_test: str = "grammer_dataset/grammar_validation.csv"
+    dataset: str = "cnn_dailymail"
     fp16: bool=False
 
     
