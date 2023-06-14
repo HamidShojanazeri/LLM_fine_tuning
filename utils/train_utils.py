c3d9ee5ecdb1ccb49eb4bf4ad12db763a5bac38e
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-14 06:58:14 +0000

    clean up

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -35,97 +35,121 @@ def byte2mb(x):
     return int(x / 2**20)
 
 def train(model, train_dataloader, optimizer, lr_scheduler, gradient_accumulation_steps, num_epochs, local_rank, train_config):
+    """
+    Trains the model on the given dataloader
+    
+    Args:
+        model: The model to be trained
+        train_dataloader: The dataloader containing the training data
+        optimizer: The optimizer used for training
+        lr_scheduler: The learning rate scheduler
+        gradient_accumulation_steps: The number of steps to accumulate gradients before performing a backward/update operation
+        num_epochs: The number of epochs to train for
+        local_rank: The rank of the current node in a distributed setting
+        train_config: The training configuration
+    
+    Returns: train_perplexity, train_epoch_loss
+    """
+    # Create a gradient scaler for fp16
+    scaler = torch.cuda.amp.GradScaler() if train_config.fp16 else None
+
     for epoch in range(num_epochs):
-        with MemoryTrace() as memtrace:
+        with MemoryTrace() as memtrace:  # track the memory usage
             model.train()
-            total_loss = 0
-            # for step, (examples, labels, example_mask) in enumerate(tqdm(train_dataloader)):
-            #     print(f"================== type(batch) : {type(examples)}, len(batch): {len(examples)}, actual example {examples.size()}===================")
-            #     inputs = {'input_ids': examples, 'attention_mask': example_mask, 'labels': labels}
+            total_loss = 0.0
+
             for step, batch in enumerate(tqdm(train_dataloader)):
-                for key in batch.keys():
-                    batch[key] = batch[key].to(local_rank)
-                outputs = model(input_ids=batch["source_ids"],attention_mask=batch["source_mask"], labels=batch["target_ids"])
+                # Move batch to device only if necessary
+                if batch['source_ids'].device != local_rank:
+                    for key in batch.keys():
+                        batch[key] = batch[key].to(local_rank)
+                
+                outputs = model(input_ids=batch["source_ids"], attention_mask=batch["source_mask"], labels=batch["target_ids"])
                 loss = outputs.loss
                 total_loss += loss.detach().float()
                 loss = loss / gradient_accumulation_steps
+                
                 if train_config.fp16:
+                    # if fp16 is enabled, use gradient scaler to handle gradient update
                     scaler.scale(loss).backward()
                     scaler.step(optimizer)
                     scaler.update()
-                loss.backward()
-                if (step+1)% gradient_accumulation_steps == 0 or step == len(train_dataloader) - 1:
+                else:
+                    # regular backpropagation when fp16 is not used
+                    loss.backward()
+
+                if (step + 1) % gradient_accumulation_steps == 0 or step == len(train_dataloader) - 1:
+                    # perform optimization step if enough gradients have been accumulated
                     optimizer.step()
                     lr_scheduler.step()
                     optimizer.zero_grad()
-        # Printing the GPU memory usage details such as allocated memory, peak memory, and total memory usage
+
+        #Printing the GPU and CPU memory usage details
         print("GPU Memory before entering the train : {}".format(byte2mb(memtrace.begin)))
         print("GPU Memory consumed at the end of the train (end-begin): {}".format(memtrace.used))
         print("GPU Peak Memory consumed during the train (max-begin): {}".format(memtrace.peaked))
-        print(
-            "GPU Total Peak Memory consumed during the train (max): {}".format(
-                memtrace.peaked + byte2mb(memtrace.begin)
-            )
-        )
-
+        print("GPU Total Peak Memory consumed during the train (max): {}".format(memtrace.peaked + byte2mb(memtrace.begin)))
         print("CPU Memory before entering the train : {}".format(byte2mb(memtrace.cpu_begin)))
         print("CPU Memory consumed at the end of the train (end-begin): {}".format(memtrace.cpu_used))
         print("CPU Peak Memory consumed during the train (max-begin): {}".format(memtrace.cpu_peaked))
-        print(
-                "CPU Total Peak Memory consumed during the train (max): {}".format(
-                    memtrace.cpu_peaked + byte2mb(memtrace.cpu_begin)
-                )
-            )
-    if torch.cuda.device_count()>1:
-        dist.all_reduce(total_loss, op=dist.ReduceOp.SUM)
-    train_epoch_loss = total_loss / len(train_dataloader)
-    train_perplexity = torch.exp(train_epoch_loss)
-    print(f"Epoch {epoch+1}: train_perplexity={train_perplexity:.4f}, train_epoch_loss={train_epoch_loss:.4f}")
-       
-def evaluation(model, eval_dataloader,local_rank ):
+        print("CPU Total Peak Memory consumed during the train (max): {}".format(memtrace.cpu_peaked + byte2mb(memtrace.cpu_begin)))
+
+        # Reducing total_loss across all devices if there's more than one CUDA device
+        if torch.cuda.device_count() > 1:
+            dist.all_reduce(total_loss, op=dist.ReduceOp.SUM)
+        
+        train_epoch_loss = total_loss / len(train_dataloader)
+        train_perplexity = torch.exp(train_epoch_loss)
+        
+        print(f"Epoch {epoch+1}: train_perplexity={train_perplexity:.4f}, train_epoch_loss={train_epoch_loss:.4f}")
+        return train_perplexity, train_epoch_loss
+
+   
+def evaluation(model, eval_dataloader, local_rank, tokenizer):
+    """
+    Evaluates the model on the given dataloader
+    
+    Args:
+        model: The model to evaluate
+        eval_dataloader: The dataloader containing the evaluation data
+        local_rank: The rank of the current node in a distributed setting
+        tokenizer: The tokenizer used to decode predictions
+    
+    Returns: eval_ppl, eval_epoch_loss
+    """
     model.eval()
     eval_preds = []
-    metric = 0.0
-    n_toks = 0
-    with MemoryTrace() as memtrace:
-        # for _, (examples, labels, example_mask) in enumerate(tqdm(eval_dataloader)):
-        for step, batch in enumerate(tqdm(eval_dataloader)):
-            for key in batch.keys():
-                batch[key] = batch[key].to(local_rank)
-            with torch.no_grad():
-                model.forward(input_ids=batch["source_ids"])
-                pred = model.generate(
-                    input_ids=batch["source_ids"], max_length=10
-                )  
-            batch["target_ids"] = batch["target_ids"].float()
-            pred = pred.float()
-            loss = F.cross_entropy(pred.flatten(0, 1), batch["target_ids"].flatten(0, 1), reduction="sum")
-            metric += loss.item()
-            n_toks += batch["target_ids"].nelement()
-
-
-    # Printing the GPU memory usage details such as allocated memory, peak memory, and total memory usage
-    print("GPU Memory before entering the eval : {}".format(byte2mb(memtrace.begin)))
-    print("GPU Memory consumed at the end of the eval (end-begin): {}".format(memtrace.used))
-    print("GPU Peak Memory consumed during the eval (max-begin): {}".format(memtrace.peaked))
-    print(
-        "GPU Total Peak Memory consumed during the eval (max): {}".format(
-            memtrace.peaked + byte2mb(memtrace.begin)
-        )
-    )
-
-    print("CPU Memory before entering the eval : {}".format(byte2mb(memtrace.cpu_begin)))
-    print("CPU Memory consumed at the end of the eval (end-begin): {}".format(memtrace.cpu_used))
-    print("CPU Peak Memory consumed during the eval (max-begin): {}".format(memtrace.cpu_peaked))
-    print(
-        "CPU Total Peak Memory consumed during the eval (max): {}".format(
-            memtrace.cpu_peaked + byte2mb(memtrace.cpu_begin)
-        )
-    )
-    if torch.device_count()>1:
-        dist.all_reduce(metric, op=dist.ReduceOp.SUM)
-    eval_perplexity = torch.exp(torch.tensor(metric / n_toks))
-    print(f"Evaluation perplexity: {eval_perplexity:.4f}")
-    return eval_perplexity
+    eval_loss = 0.0  # Initialize evaluation loss
     
-   
+    with MemoryTrace() as memtrace:
+        for step, batch in enumerate(tqdm(eval_dataloader)):
+            # Move batch to device only if necessary
+            if batch['source_ids'].device != local_rank:
+                for key in batch.keys():
+                    batch[key] = batch[key].to(local_rank)
+                    
+            # Ensure no gradients are computed for this scope to save memory
+            with torch.no_grad():
+                # Forward pass and compute loss
+                outputs = model(input_ids=batch["source_ids"], labels= batch["target_ids"] )
+                loss = outputs.loss
+                eval_loss += loss.detach().float()
+                
+            # Decode predictions and add to evaluation predictions list
+            preds = torch.argmax(outputs.logits, -1)
+            eval_preds.extend(
+                tokenizer.batch_decode(preds.detach().cpu().numpy(), skip_special_tokens=True)
+            )
+    
+    # If there's more than one CUDA device, reduce evaluation loss across all devices
+    if torch.cuda.device_count() > 1:
+        dist.all_reduce(eval_loss, op=dist.ReduceOp.SUM)
+    
+    # Compute average loss and perplexity
+    eval_epoch_loss = eval_loss / len(eval_dataloader)
+    eval_ppl = torch.exp(eval_epoch_loss)
+    
+    # Print evaluation metrics
+    print(f" {eval_ppl=} {eval_epoch_loss=}")
+    return eval_ppl, eval_epoch_loss
+ 
