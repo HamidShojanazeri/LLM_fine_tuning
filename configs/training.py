4743883be2d1909a467cdd1c14e86110ef3d7ffa
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-16 15:38:54 +0000

    update config to align with readme

--- configs/training.py
+++ configs/training.py
@@ -5,7 +5,7 @@ from typing import ClassVar
 @dataclass
 class train_config:
     model_name: str="decapoda-research/llama-7b-hf"
-    train_strategy: str="not_fsdp" # not_fsdp
+    enable_fsdp: bool= True 
     run_validation: bool=True
     batch_size_training: int=4
     num_epochs: int=3
@@ -20,10 +20,12 @@ class train_config:
     fp16: bool=False
     micro_batch_size: int=1
     peft_method: str = "lora" # None , llama_adapter, prefix
+    use_peft: bool=False
     model_path: str=""
     data_path: str = "path to alpaca data json file"
     output_dir: str = "./ft-output"
     freeze_layers: bool = False
+    num_freeze_layers: int = 1
     quantization: bool = False
     one_gpu: bool = False
     
