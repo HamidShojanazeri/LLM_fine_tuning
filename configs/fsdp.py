86bbfa2142bd3271a18851601d63a165fc7cfce2
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-16 00:28:58 +0000

    making bf16 default

--- configs/fsdp.py
+++ configs/fsdp.py
@@ -5,11 +5,11 @@ from torch.distributed.fsdp import ShardingStrategy
 @dataclass
 class fsdp_config:
     mixed_precision: bool=True
-    use_fp16: bool=True
+    use_fp16: bool=False
     seed: int=42
     sharding_strategy: ShardingStrategy = ShardingStrategy.FULL_SHARD
     fsdp_activation_checkpointing: bool=True
-    pure_bf16: bool = False
+    pure_bf16: bool = True
     optimizer: str= "anyprecision"
     
     
