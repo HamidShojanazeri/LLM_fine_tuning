1d0a6d15b5e793ac56c9b2f2a4894cf3d6bc36f0
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-16 00:28:30 +0000

    fixing the path and names

--- configs/datasets.py
+++ configs/datasets.py
@@ -2,19 +2,19 @@ from dataclasses import dataclass
 
 @dataclass
 class cnn_dailymail_dataset:
-    dataset: str =  "cnn_dailymail"
+    dataset: str =  "cnn_dailymail_dataset"
     train_split: str = "train[0:100]"
     test_split: str = "validation[0:100]"
     
 @dataclass
 class grammar_dataset:
     dataset: str = "grammar_dataset"
-    train_split: str = "grammer_dataset/gtrain_1k.csv"  # grammar_13k.csv
-    test_split: str = "grammer_dataset/grammar_validation.csv"
+    train_split: str = "ft_datasets/grammar_dataset/gtrain_1k.csv"  # grammar_13k.csv
+    test_split: str = "ft_datasets/grammar_dataset/grammar_validation.csv"
     
 @dataclass
 class alpaca_dataset:
-    dataset: str = "alpaca"
+    dataset: str = "alpaca_dataset"
     train_split: str = "train"
     test_split: str = "val"
     data_path: str = "/data/home/hamidnazeri/stanford_alpaca/alpaca_data.json"
