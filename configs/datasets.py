951bf33862998ba918ce916d5428adb33360f200
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-21 21:42:20 +0000

    fixing alpaca data path

--- configs/datasets.py
+++ configs/datasets.py
@@ -23,5 +23,4 @@ class alpaca_dataset:
     dataset: str = "alpaca_dataset"
     train_split: str = "train"
     test_split: str = "val"
-    data_path: str = "/data/home/hamidnazeri/stanford_alpaca/alpaca_data.json"
-    model_path = "/data/home/hamidnazeri/LLM_fine_tuning/model/models--decapoda-research--llama-7b-hf/snapshots/5f98eefcc80e437ef68d457ad7bf167c2c6a1348/"
+    data_path: str = "ft_datasets/alpaca_data.json"
