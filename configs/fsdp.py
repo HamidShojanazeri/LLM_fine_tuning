4b8d4c39191c01d91e30ac12fb7ec9728fd5bcd4
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-21 00:50:31 +0000

    changing the default for state_dict to full as sharded not working on nightlies

--- configs/fsdp.py
+++ configs/fsdp.py
@@ -9,7 +9,7 @@ class fsdp_config:
     use_fp16: bool=False
     seed: int=42
     sharding_strategy: ShardingStrategy = ShardingStrategy.FULL_SHARD
-    checkpoint_type: StateDictType = StateDictType.SHARDED_STATE_DICT # alternatively can use FULL_STATE_DICT to assemble the full checkpoints on CPU.
+    checkpoint_type: StateDictType = StateDictType.FULL_STATE_DICT  # alternatively can use SHARDED_STATE_DICT save one file per rank, and can resize the world-size.
     fsdp_activation_checkpointing: bool=True
     pure_bf16: bool = True
     optimizer: str= "anyprecision"
