c15326603ddf6ccf4c798e18bf9607c51ac22523
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-21 21:43:02 +0000

    setting pure BF16 to fasle

--- configs/fsdp.py
+++ configs/fsdp.py
@@ -11,7 +11,7 @@ class fsdp_config:
     sharding_strategy: ShardingStrategy = ShardingStrategy.FULL_SHARD
     checkpoint_type: StateDictType = StateDictType.FULL_STATE_DICT  # alternatively can use SHARDED_STATE_DICT save one file per rank, and can resize the world-size.
     fsdp_activation_checkpointing: bool=True
-    pure_bf16: bool = True
+    pure_bf16: bool = False
     optimizer: str= "anyprecision"
     
     
