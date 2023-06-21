2880131c7014826124c2518164e51b51752d8003
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-21 00:50:03 +0000

    changing the model to causal LM

--- LLaMA-english_quote_inference.py
+++ LLaMA-english_quote_inference.py
@@ -2,25 +2,25 @@ import torch
 from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaTokenizer, LlamaForSequenceClassification
 
-# peft_model_id = "ybelkada/opt-6.7b-lora"
-peft_model_id = "llama-adapter-imdb-4epochs-lr14e4"
+peft_model_id = "ybelkada/opt-6.7b-lora"
+# peft_model_id = "llama-adapter-imdb-4epochs-lr14e4"
 config = PeftConfig.from_pretrained(peft_model_id)
-model = LlamaForSequenceClassification.from_pretrained(
+model = AutoModelForCausalLM.from_pretrained(
     "decapoda-research/llama-7b-hf", return_dict=True, load_in_8bit=True, device_map="auto"
 )
-tokenizer = LlamaTokenizer.from_pretrained(config.base_model_name_or_path)
+tokenizer = LlamaTokenizer.from_pretrained("decapoda-research/llama-7b-hf")
 
 #Load the Lora model
 model = PeftModel.from_pretrained(model, peft_model_id)
 
-batch = tokenizer("The movie could be really better.", return_tensors="pt")
+batch = tokenizer("The following is a list of companies and the categories they fall into:\n Apple, Facebook, Fedex\n Apple Category:", return_tensors="pt")
 
-with torch.no_grad():
-        outputs = model(**batch)
-        predictions = outputs.logits.argmax(dim=-1)
-        print(predictions)
+# with torch.no_grad():
+#         outputs = model(**batch)
+#         predictions = outputs.logits.argmax(dim=-1)
+#         print(predictions)
     
-# with torch.cuda.amp.autocast():
-#     output_tokens = model.generate(**batch, max_new_tokens=10, eos_token_id=3)
-# print("output_tokens", output_tokens)
-# print("\n\n", tokenizer.decode(output_tokens[0], skip_special_tokens=True))
+with torch.cuda.amp.autocast():
+    output_tokens = model.generate(**batch, max_new_tokens=10, eos_token_id=3)
+print("output_tokens", output_tokens)
+print("\n\n", tokenizer.decode(output_tokens[0], skip_special_tokens=True))
