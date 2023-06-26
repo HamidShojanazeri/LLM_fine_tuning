57552a8fdb21b12383ac8e4f181234afe6c51824
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2023-06-26 20:31:05 +0000

    changing default optimizer

--- configs/fsdp.py
+++ configs/fsdp.py
@@ -14,7 +14,7 @@ class fsdp_config:
     checkpoint_type: StateDictType = StateDictType.SHARDED_STATE_DICT  # alternatively can use SHARDED_STATE_DICT save one file per rank, and can resize the world-size.
     fsdp_activation_checkpointing: bool=True
     pure_bf16: bool = False
-    optimizer: str= "anyprecision"
+    optimizer: str= "AdamW"
     
     
     
