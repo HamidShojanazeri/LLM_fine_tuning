646b68a4551c94662a5f63b9148461cb1f612c1b
Matthias Reso <13337103+mreso@users.noreply.github.com>
2023-06-29 22:12:26 +0000

    Convert grammar into struction learning dataset + introduced configurable input length to samsum, cnn_dm + grammar

--- ft_datasets/utils.py
+++ ft_datasets/utils.py
@@ -1,6 +1,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
+from tqdm import tqdm
+from torch.utils.data import Dataset
 from itertools import chain
 
 
@@ -36,3 +38,30 @@ class Concatenator(object):
         result["labels"] = result["input_ids"].copy()
 
         return result
+    
+
+class ConcatDataset(Dataset):
+    def __init__(self, dataset, chunk_size=4096):
+        self.dataset = dataset
+        self.chunk_size = chunk_size
+        
+        self.samples = []
+        
+        buffer = {
+            "input_ids": [],
+            "attention_mask": [],
+            "labels": [],
+            }
+        
+        for sample in tqdm(self.dataset, desc="Preprocessing dataset"):
+            buffer = {k: v + sample[k] for k,v in buffer.items()}
+            
+            while len(next(iter(buffer.values()))) > self.chunk_size:
+                self.samples.append({k: v[:self.chunk_size] for k,v in buffer.items()})
+                buffer = {k: v[self.chunk_size:] for k,v in buffer.items()}
+                
+    def __getitem__(self, idx):
+        return self.samples[idx]
+    
+    def __len__(self):
+        return len(self.samples)
